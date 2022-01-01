f09bb833ac0987533ca51c477bcd1a0d29d051ba
karishmarajput <https://github.com/karishmarajput>
2022-01-01 14:24:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import LoginForm from './components/LoginForm';
+import { ChatEngine } from 'react-chat-engine';
 
-function App() {
+
+const projectID='20eb15fa-2447-4808-aecf-34cfdd1ee771';
+
+const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
   );
-}
+};
 
 export default App;
